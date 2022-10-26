9d3a0d88b540604e22575ed5214a3edc7d8e99d6
Fred Suh <50889806+heessuh@users.noreply.github.com>
2022-10-26 16:24:51 -0300

    added current guess features

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
   if (guess) {
     return (
       <div className="row past">
@@ -8,6 +8,22 @@ export default function Row({ guess }) {
           <div key={i} className={l.color}>
             {l.key}
           </div>
+        ))}
+      </div>
+    )
+  }
+  if (currentGuess) {
+    let letters = currentGuess.split("")
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     )
